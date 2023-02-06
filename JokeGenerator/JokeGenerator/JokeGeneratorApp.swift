4f95fd4844538ed357584213407cde3ea296899b
fraander <57777918+fraander@users.noreply.github.com>
2023-02-06 12:34:33 -0500

    create xcode project

---
+++ JokeGenerator/JokeGenerator/JokeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeGeneratorApp.swift
+//  JokeGenerator
+//
+//  Created by Frank Anderson on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct JokeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
